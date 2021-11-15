c7b07b10e6376491cf5cc8e0fac7f447a0ed367c
Solia-dove <solime27@gmail.com>
2021-11-14 20:49:23 -0600

    frontend

--- Frontend/SearchJobAngular/src/app/app.component.ts
+++ Frontend/SearchJobAngular/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'SearchJobAngular';
+  tiles=[
+    {text: 'Searchbar', cols: 3, rows: 2, color: 'lightblue'},
+    {text: 'Recent Jobs', cols: 1, rows: 6, color: 'lightgreen'},
+    {text: 'Job Search Result', cols: 1, rows: 7, color: 'lightpink'},
+    {text: 'Job Details', cols: 2, rows: 7, color: '#DDBDF1'},
+  ];
 }
